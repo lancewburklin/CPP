2e996461db90b12d7e43afdb1eea576bd5f3b034
lancewburklin <lance.burklin@atlasschool.com>
2024-04-05 10:54:43 -0600

    Adding sum_them_all function

---
+++ Variadic Functions/sum_them_all.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdarg>
+
+/*
+	sum_them_all: Sums inputs
+	Parameters:
+		n: Numbers to sum
+	Returns: The result
+*/
+
+int sum_them_all(const unsigned int n, ...) {
+	va_list args;
+	va_start(args, n);
+	int count = 0;
+
+	for (unsigned int i = 0; i < n; i++) {
+		count += va_arg(args, int);
+	}
+	va_end(args);
+	return count;
+}
+
+int test_sum_them_all()
+{
+	int sum;
+
+	sum = sum_them_all(2, 98, 1024);
+	std::cout << sum << std::endl;
+	sum = sum_them_all(4, 98, 1024, 402, -1024);
+	std::cout << sum << std::endl;
+	sum = sum_them_all(0);
+	std::cout << sum << std::endl;
+	return (0);
+}
